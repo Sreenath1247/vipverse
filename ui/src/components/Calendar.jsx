aa3a6c4c53d42fa84a7d7a64974f2c0a9ad1d872
Sreenath1247 <sreenath1247@gmail.com>
2025-05-07 13:46:09 +0530

    game calendar layout changes

--- ui/src/components/Calendar.jsx
+++ ui/src/components/Calendar.jsx
@@ -10,32 +10,29 @@ const Calendar = ({ list }) => {
       groupedData[monthYear] = [];
     }
     groupedData[monthYear].push(item);
-    // console.log(moment(item.date).format("MMM Do YYYY"));
   });
 
   return (
-    <div className="flex flex-col items-center w-full">
+    <div className="flex flex-col w-full">
       {Object.keys(groupedData).map((item) => (
         <div key={item}>
-          <p className="text-4xl font-bold">
+          <p className="text-4xl font-bold mt-5 lg:text-left sm:text-center text-center">
             {moment(item, "YYYY-MM").format("MMMM")}
           </p>
-          <div className="grid grid-cols-1 gap-4 sm:gap-6 lg:grid-cols-2 xl:grid-cols-4">
+          <div className="grid grid-cols-1 gap-6 lg:grid-cols-1 xl:grid-cols-2 mt-10 mb-5">
             {groupedData[item].map((game, index) => (
-              <div
-                key={index}
-                className="hover:cursor-pointer transform transition-transform duration-300 hover:scale-105"
-              >
-                <div className="flex flex-col items-center justify-center bg-gray-800 rounded-lg shadow-lg w-full h-[350px]">
+              <div key={index} className="flex flex-row hover:cursor-pointer transform transition-transform duration-300 hover:scale-105">
+                <div>
                   <img
                     src={game.image}
                     alt={game.name}
-                    className="w-full h-full object-cover rounded-lg"
+                    className="w-100 h-80 object-cover rounded-lg rounded-r-none"
                   />
                 </div>
-                <h2 className="text-white text-lg font-bold mt-2 text-center">
-                  {game.name}
-                </h2>
+                <div className="flex flex-col bg-slate-800 rounded-r w-80 p-10 text-lg">
+                  <div className="text-3xl mb-2 font-bold">{game.name}</div>
+                  <div>{moment(game.date).format("MMM Do YYYY")}</div>
+                </div>
               </div>
             ))}
           </div>
